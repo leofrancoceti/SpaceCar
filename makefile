c643905f398f72027a91d7070af375e5dc381e47
LeonelGlezHdez <160064590+LeonelGlezHdez@users.noreply.github.com>
2024-05-14 18:16:48 +0000

    Version 5

--- makefile
+++ makefile
@@ -1,1 +1,5 @@
+bin/draw : src/dibujo_test.cpp
+	g++ src/dibujo_test.cpp -o bin/draw -I include -l ftxui-screen -l ftxui-dom -l ftxui-component -std=c++2a
 
+run : bin/draw
+	./bin/draw
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,7 @@
-#pragma once
 #include <iostream>
-#include <string>
-#include <thread>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/screen/string.hpp>
-#include <ftxui/screen/terminal.hpp>
-#include <fstream>
-#include <Dibujo.hpp>
-#include <Archivo.hpp>
-
-using namespace std;
-using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    Archivo carro("./assets/Carro.txt");
-    
-
-
-   
-
-  
+    std::cout << "ARRIBA LAS AGUILAS DEL AMERICA" << std::endl;
+    return 0;
 }
